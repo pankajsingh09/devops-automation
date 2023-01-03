e846690ca503c14d96d91e473c01520fa2239b68
Pankaj Singh <pankaj.singh04@nagarro.com>
2023-01-03 22:30:50 +0530

    changes made in Homecontroller

--- src/main/java/com/HomeController.java
+++ src/main/java/com/HomeController.java
@@ -14,10 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
-
-
-
 @RestController
 public class HomeController {
 	
